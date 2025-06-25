5feb66eb2c1b4b46b00a23e00547759537088c17
Luis Felipe Trega Cipriano <luistreguinha@gmail.com>
2025-06-25 20:26:03 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
-#include "convertein_posfixa.h"
 #include "expressao.h"
 
 
-int main(void) {
-    /*printf("Digite a Expressao Infixa: ");
-    char* resultadoPosfixa = recebe_infixa();
-    double resultado = resolveRPN(resultadoPosfixa);
+void executarOperacoesDeTeste() {
+    printf("\n--- Executando operacoes de teste ---\n");
 
-    printf("Valor RPN: %.2f\n", resultado);*/
-
-    char *expressoesPosfixas[23] = {
+    char *expressoesPosfixas[33] = {
         "-5 3 +",
+        "-5 + 3",
+        "2 * -4",
         "2 -4 *",
+        "(-1) * (-2)",
         "-1 -2 *",
+        "3.5 + 2.5",
         "3.5 2.5 +",
         "5.5 10.2 -",
+        "5.5 - 10.2",
         "6 2.0 /",
+        "6 / 2.0",
+        "4 / 0",
         "4 0 /",
+        "(-3.3) * 2",
         "-3.3 2 *",
+        "(7 - 3.5) / 3.5",
         "7 3.5 - 3.5 /",
+        "((200+3) * (-1.5))",
         "2 3 + -1.5 *",
         "2 3 ^", // 8
         "16 sqrt", // 4
@@ -38,14 +44,65 @@ int main(void) {
         "3 4 + 1 * 5 sen +"
     };
 
-    //calcularPosfixa(resultadoPosfixa);  // Sua função da calculadora
-    for (int i = 0; i < 23; i++) {
-        printf("Testando expressao pos-fixa: %s\n", expressoesPosfixas[i]);
+    for (int i = 0; i < 33; i++) {
+        printf("Testando expressao: %s\n", expressoesPosfixas[i]);
         double resultado = resolveRPN(expressoesPosfixas[i]);
         printf("Resultado: %.2f\n\n", resultado);
     }
+}
+
+void entrarComExpressao(char etipo) {
+	printf("Digite a Expressao: ");
+	//valida o tipo da expressão e então converte e resolve a expressão
+	if(etipo == 'i'){
+	    char *resultadoPosfixa = recebe_infixa();
+	    double resultado = resolveRPN(resultadoPosfixa);
+	    printf("\nValor RPN: %.2f\n", resultado);
+	}else{
+		char* expr = lerExpressaoDinamica();
+		char *resultadoInfixa = recebe_posfixa(expr);
+		printf("expressão final %s\n", resultadoInfixa);
+		double resultado = resolveRPN(expr);
+		
+		
+	}
+}
 
 
-    //free(resultadoPosfixa);
+int main(void) {
+    setlocale(LC_ALL, "");
+    int opcao;
+
+    do {
+        printf("\n=========== CALCULADORA POLONESA ==========\n");
+		printf("1 - Entrar com expressao infixada\n");
+        printf("2 - Entrar com expressao pós-fixada\n");
+        printf("3 - Executar operacoes de teste\n");
+        printf("4 - Sair\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar();
+
+        switch (opcao) {
+			case 1:
+				entrarComExpressao('i');
+				//i para infixada
+				break;
+            case 2:
+                entrarComExpressao('p');
+				//p para pós fixada
+                break;
+            case 3:
+                executarOperacoesDeTeste();
+                break;
+            case 4:
+                printf("Saindo... Valeu, campeão!\n");
+                break;
+            default:
+                printf("Opcao invalida! Tente novamente.\n");
+        }
+
+    } while (opcao != 3);
+
     return 0;
 }
